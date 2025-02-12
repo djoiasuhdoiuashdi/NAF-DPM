ca78130c47d79dfe8f38149ede8a3d41abbd1786
dom <dominic.akt@tuwien.ac.at>
2025-02-12 11:17:09 +0100

    test

--- Binarization/src/trainer.py
+++ Binarization/src/trainer.py
@@ -247,8 +247,8 @@ class Trainer:
 
                 im_pil = Image.fromarray(im)
                 im_pil.save(name_str+".tiff")
-                im_gt = Image.fromarray(im_gt)
-                im_gt.save(name_str +".tiff")
+                im_gt_pil = Image.fromarray(im_gt)
+                im_gt_pil.save(name_str +".tiff")
                 # print(f"im_binary - Max: {np.max(im)}, Min: {np.min(im)}")
                 # print(f"im_gt_binary - Max: {np.max(im_gt)}, Min: {np.min(im_gt)}")
 
